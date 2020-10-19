23c231f2b6e88fe965ed2bc727ae521fb8c55506
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-10-19 15:47:45 +0000

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,11 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	for (i = 0; i < size-1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size-i-1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
@@ -25,8 +25,8 @@ void bubble_sort(int *array, size_t size)
 
 /**
  * swap - swaps the values of two integers
- * @a: pointer to int to be swapped
- * @b: pointer to other int to be swapped
+ * @xp: pointer to int to be swapped
+ * @yp: pointer to other int to be swapped
  */
 void swap(int *xp, int *yp)
 {
