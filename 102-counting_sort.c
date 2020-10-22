da5ea4bd59bb4e23affe48fa99b9eb9cc3c89955
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-10-22 00:09:06 +0000

    Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -17,24 +17,28 @@ void counting_sort(int *array, size_t size)
 		if (array[i] > max)
 			max = array[i];
 	max = max + 1;
-	count = malloc((size + 1) * sizeof(int));
+	count = calloc((size + 1), sizeof(int));
 	if (count == NULL)
 		return;
 
-	output = malloc((max + 1) * sizeof(int));
+	output = calloc((max + 1), sizeof(int));
 	if (output == NULL)
 		return;
 
 	for (i = 0; i < (int)size; i++)
 		output[array[i]] = output[array[i]] + 1;
+
 	for (j = 0; j < max; j++)
 		output[j + 1] = output[j] + output[j + 1];
+
 	print_array(output, max);
+
 	for (i = 0; i < (int)size; i++)
 	{
 		count[output[array[i]] - 1] = array[i];
 		output[array[i]] -= 1;
 	}
+
 	for (i = 0; i < (int)size; i++)
 		array[i] = count[i];
 
